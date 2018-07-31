0b4e5cce91c5b0b41af6c929ee7aca5082811363
Rob Taylor <roboncode@gmail.com>
2018-07-31 13:34:20 -0600

    updated demo

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,43 @@
-const migrations = require('./migrations')
-const models = require('./models')
+const fs = require('fs')
+const path = require('path')
+const rootPath = path.join(__dirname, '..')
+const arango = require(path.join(rootPath, 'arango'))
+const {
+  importAllDocs
+} = require(path.join(__dirname, 'migrations'))
+require('colors')
 
-module.exports = {
-  migrations,
-  models,
+arango.events.on('connected', () => {
+  console.log('Arango is connected!'.green)
+})
+
+function readFiles(dir) {
+  let files = fs.readdirSync(dir)
+  for (var i = 0; i < files.length; i++) {
+    require(dir + '/' + files[i])
+  }
 }
+
+async function main() {
+  // Initialize models
+  readFiles(path.join(__dirname, 'models'))
+
+  // Create connection
+  const conn = await arango.connect({
+    name: 'demo'
+  })
+
+  // Initialize database
+  await arango.initDatabase(conn, arango.models)
+
+  // Import migration docs
+  await importAllDocs(conn)
+
+  const User = arango.model('User')
+  let user = await User
+    // .setConnection(conn)
+    .findById('rob')
+  console.log('#user', user)
+}
+
+main()
