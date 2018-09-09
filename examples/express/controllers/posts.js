0f3f93fe6c5f77bd7290443b889f69df553275ed
Rob Taylor <roboncode@gmail.com>
2018-09-09 15:17:05 -0600

    updated posts example

--- examples/express/controllers/posts.js
+++ examples/express/controllers/posts.js
@@ -4,15 +4,26 @@ const Post = orango.model('Post')
 const User = orango.model('User')
 
 app.get('/posts', async (req, res) => {
-  const posts = await Post.findMany()
-  res.send(posts)
+  const authorId = req.query.author
+  if (authorId) {
+    try {
+      let posts = await Post.getPostsByAuthor(authorId)
+      res.send(posts)
+    } catch (e) {
+      res.send('error: ' + e.message)
+    }
+  } else {
+    const posts = await Post.findMany(req.query)
+    .populate('user', User)
+    res.send(posts)
+  }
 })
 
 app.get('/posts/:id', async (req, res) => {
   try {
     let posts = await Post.getPostsByAuthor(req.params.id)
     res.send(posts)
-  } catch(e) {
+  } catch (e) {
     res.send('error: ' + e.message)
   }
 })
