19c86d515e36fe26042504285091f001ddea0ab5
Rob Taylor <roboncode@gmail.com>
2018-09-10 08:04:20 -0600

    updated helpers

--- examples/express/helpers/readFiles.js
+++ examples/express/helpers/readFiles.js
@@ -4,7 +4,7 @@ const path = require('path')
 function readFiles(dir) {
   let files = fs.readdirSync(dir)
   for (var i = 0; i < files.length; i++) {
-    require(path.join('..', dir, files[i]))
+    require(path.join(dir, files[i]))
   }
 }
 
