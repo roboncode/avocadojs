c8942b97ba597afdda4a9369f782e098fa380645
Rob Taylor <roboncode@gmail.com>
2018-09-25 10:30:53 -0600

    fixed edge imports

--- examples/express/tools/sampleData/data/likes.js
+++ examples/express/tools/sampleData/data/likes.js
@@ -1,16 +1,7 @@
 const likes = [
-  {
-    _from: 'posts/first',
-    _to: 'users/rob'
-  },
-  {
-    _from: 'posts/first',
-    _to: 'users/chase'
-  },
-  {
-    _from: 'posts/another',
-    _to: 'users/rob'
-  }
+  { posts: 'first', users: 'rob' },
+  { posts: 'first', users: 'chase' },
+  { posts: 'another', users: 'rob' }
 ]
 
 module.exports = likes
