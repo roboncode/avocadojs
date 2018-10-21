c7a02ff111c3f6ed2b8729a5473820832b572ef3
Rob Taylor <roboncode@gmail.com>
2018-10-20 18:12:07 -0600

    updated packages

--- examples/client/vue.config.js
+++ examples/client/vue.config.js
@@ -1,6 +1,12 @@
+// const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 module.exports = {
   devServer: {
     proxy: 'http://localhost:3000'
   },
-  runtimeCompiler: true
+  // runtimeCompiler: true,
+  // configureWebpack: {
+  //   plugins: [
+  //     new VuetifyLoaderPlugin()
+  //   ]
+  // }
 }
