851843e9d0ece1ac7f67b37fc59ce40be4dedcda
Rob Taylor <roboncode@gmail.com>
2018-11-19 08:11:52 -0700

    fixed refs to orango in snippets

--- examples/snippets/models/User.js
+++ examples/snippets/models/User.js
@@ -1,27 +1,36 @@
-const orango = require('../../lib')
+const orango = require('../../../lib')
 
-const UserSchema = orango.Schema({
-  name: String,
-  email: { type: String, required: 'create' },
-  settings: {
-    locale: {
-      type: String,
-      default: 'en_US'
-    }
+const UserSchema = orango.Schema(
+  {
+    name: String,
+    email: { type: String, required: 'create' },
+    settings: {
+      locale: {
+        type: String,
+        default: 'en_US'
+      }
+    },
+    stats: {
+      friends: { type: orango.Types.Any, default: 0 },
+      posts: { type: orango.Types.Any, default: 0 }
+    },
+    created: Date,
+    updated: Date
   },
-  stats: {
-    friends: { type: orango.Types.Any, default: 0 },
-    posts: { type: orango.Types.Any, default: 0 }
-  },
-  created: Date,
-  updated: Date
-}, {
-  strict: true,
-  indexes: [{
-    type: 'hash',
-    fields: ['email']
-  }]
-})
+  {
+    strict: true,
+    indexes: [
+      {
+        type: 'hash',
+        fields: [ 'email' ]
+      }
+    ]
+  }
+)
+
+UserSchema.computed.username = function(payload) {
+  return this.email.replace(/(\w+)@(\w+).*/g, '$1_$2')
+}
 
 const User = orango.model('User', UserSchema, 'users')
 
