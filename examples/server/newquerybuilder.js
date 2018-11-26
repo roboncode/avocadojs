a31bb6782a09fd06257284548f333ffa8c3aa48b
Rob Taylor <roboncode@gmail.com>
2018-11-26 14:30:57 -0700

    testing merge and keep in qb

--- examples/server/newquerybuilder.js
+++ examples/server/newquerybuilder.js
@@ -2,7 +2,13 @@ require('app-module-path').addPath(__dirname)
 const orango = require('orango')
 const readFiles = require('./helpers/readFiles')
 const pluralize = require('pluralize')
-const { sortToAQL, filterToAQL, setDefaultsToNull, arrayOverride, createUniqueId } = orango.helpers
+const {
+  sortToAQL,
+  filterToAQL,
+  setDefaultsToNull,
+  arrayOverride,
+  createUniqueId
+} = orango.helpers
 require('colors')
 
 const AQB = orango.AQB
@@ -12,8 +18,17 @@ function parseQuery(data) {
   let col = ModelCls.collectionName
   let doc = pluralize.singular(col)
   let aql = AQB.for(doc).in(col)
-  let filter = filterToAQL(data.filter, doc)
-  aql = aql.filter(AQB.expr(filter))
+  let filterAQL = filterToAQL(data.filter, doc)
+  let result = doc
+  if (data.select) {
+    let select = data.select.split(' ')
+    for (let i = 0; i < select.length; i++) {
+      select[i] = AQB.str(select[i])
+    }
+    result = AQB.KEEP(doc, select)
+  }
+
+  aql = aql.filter(AQB.expr(filterAQL))
   if (data.offset && data.limit) {
     aql = aql.limit(data.offset, data.limit)
   } else if (data.offset) {
@@ -22,9 +37,22 @@ function parseQuery(data) {
     aql = aql.limit(data.limit)
   }
 
+  // dummy
+  // these are just properties???
+  let merges = ['junk', 'test']
+  // these are populates
+  let appends = ['user']
+
+  let appendData = {}
+  for(let name of appends) {
+    appendData[name] = name
+  }
+
+  result = AQB.MERGE(result, AQB.expr(merges.join(', ')), appendData)
+
   // AQB.KEEP(aqb.DOCUMENT(aqb.CONCAT(aqb.str('users/'), 'tweet.user')), AQB.str('firstName'), AQB.str('lastName'))
-  if(data.)
-  aql = aql.return(doc)
+
+  aql = aql.return(result)
   return aql.toAQL()
 }
 
@@ -34,12 +62,17 @@ async function main() {
   let result = parseQuery({
     model: 'Tweet',
     method: 'find',
-    filter: { $or: [ { active: true }, { test: 1 } ] },
+    filter: {
+      $or: [{
+        active: true
+      }, {
+        test: 1
+      }]
+    },
     limit: 10,
-    // offset: 1,
+    offset: 1,
     select: 'firstName lastName',
-    populate: [
-      {
+    populate: [{
         model: 'User',
         computed: true,
         select: 'firstName lastName'
@@ -48,7 +81,11 @@ async function main() {
         model: 'Comment',
         limit: 10,
         computed: true,
-        populate: [ { model: 'User', computed: true, select: 'firstName lastName' } ]
+        populate: [{
+          model: 'User',
+          computed: true,
+          select: 'firstName lastName'
+        }]
       }
     ],
     computed: true
