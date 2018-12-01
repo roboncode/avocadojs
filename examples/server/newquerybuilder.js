76d666bc747e8cff1cbd6fe354632abd830a0a4f
Rob Taylor <roboncode@gmail.com>
2018-12-01 01:53:30 -0700

    updates to query builder

--- examples/server/newquerybuilder.js
+++ examples/server/newquerybuilder.js
@@ -26,25 +26,42 @@ User hasOne
 User_Role
 */
 
+let counter = 1
 const AQB = orango.AQB
 
+function isOne(method) {
+  switch (method) {
+    case 'findOne':
+      return true
+  }
+  return false
+}
+
 function parseQuery(data) {
+  if (data.method === 'findOne') {
+    data.limit = 1
+  }
   let ModelCls = orango.model(data.model)
   let col = ModelCls.collectionName
-  let doc = pluralize.singular(col)
-  let aql = AQB.for(doc).in(col)
 
-  let result = doc
+  if (data.name === col) {
+    throw new Error('Name cannot be the same name as collection: ' + col)
+  }
+
+  let name = data.name || pluralize.singular(col)
+  let aql = AQB.for(name).in(col)
+
+  let result = name
   if (data.select) {
     let select = data.select.split(' ')
     for (let i = 0; i < select.length; i++) {
       select[i] = AQB.str(select[i])
     }
-    result = AQB.KEEP(doc, select)
+    result = AQB.KEEP(name, select)
   }
 
   if (data.filter) {
-    let filterAQL = filterToAQL(data.filter, doc)
+    let filterAQL = filterToAQL(data.filter, name)
     aql = aql.filter(AQB.expr(filterAQL))
     if (data.offset && data.limit) {
       aql = aql.limit(data.offset, data.limit)
@@ -68,41 +85,63 @@ function parseQuery(data) {
       const relation = ModelCls.getRelation(pop.model)
       console.log(data.model.blue, 'has', relation.has.magenta, pop.model.green)
       // console.log(pop.model, 'hasMany'.magenta, data.model, '=', PopModelCls._hasMany)
-      switch (relation.has) {
-        case RELATION.MANY:
-          aql = aql.let(PopModelCls.collectionName, parseQuery(pop))
-          appends.push(PopModelCls.collectionName)
-          break
-        case RELATION.ONE:
-          // console.log(AQB.DOCUMENT(AQB.CONCAT(AQB.str(`${col}/`), `${doc}.${relation.ref}`)).toAQL())
-          // console.log('whois', pop.filter)
-          if (pop.filter) {
-            pop.filter = merge({
-              _key: `@{${doc}.${relation.ref}}`
-            }, pop.filter)
-          } else {
-            pop.filter = {
-              _key: `@{${doc}.${relation.ref}}`
-            }
-          }
+      // switch (relation.has) {
+      //   case RELATION.MANY:
+      //     aql = aql.let(PopModelCls.collectionName, parseQuery(pop))
+      //     appends.push(PopModelCls.collectionName)
+      //     break
+      //   case RELATION.ONE:
+      //     // console.log(AQB.nameUMENT(AQB.CONCAT(AQB.str(`${col}/`), `${name}.${relation.ref}`)).toAQL())
+      //     // console.log('whois', pop.filter)
+      //     if (pop.filter) {
+      //       pop.filter = merge({
+      //         _key: `@{${name}.${relation.ref}}`
+      //       }, pop.filter)
+      //     } else {
+      //       pop.filter = {
+      //         _key: `@{${name}.${relation.ref}}`
+      //       }
+      //     }
 
-          pop.limit = 1
+      //     pop.limit = 1
 
-          aql = aql.let(relation.as, AQB.FIRST(parseQuery(pop)))
+      //     aql = aql.let(relation.as, AQB.FIRST(parseQuery(pop)))
 
-          if (pop.merge) {
-            merges.push(relation.as)
-          } else {
-            appends.push(relation.as)
-          }
-          break
+      //     if (pop.merge) {
+      //       merges.push(relation.as)
+      //     } else {
+      //       appends.push(relation.as)
+      //     }
+      //     break
+      // }
+
+      // let name = pop.name || pluralize.singular(data.model.charAt(0).toLowerCase() + data.model.slice(1))
+      // let name = isOne(pop.method) ? pluralize.singular(pop.model.charAt(0).toLowerCase() + pop.model.slice(1)) : PopModelCls.collectionName
+      // let name = pop.name
+      // if(!name) {
+      //   name = isOne(pop.method) ? pluralize.singular(pop.model.charAt(0).toLowerCase() + pop.model.slice(1)) : PopModelCls.collectionName
+      // }
+      let popName = pop.name || PopModelCls.collectionName
+      if (isOne(pop.method)) {
+        aql = aql.let(popName, AQB.FIRST(parseQuery(pop)))
+      } else {
+        aql = aql.let(popName, parseQuery(pop))
+      }
+
+      if (pop.merge) {
+        merges.push(popName)
+      } else {
+        appends.push({
+          key: pop.append || popName,
+          value: popName
+        })
       }
     }
   }
 
   let appendData = {}
-  for (let name of appends) {
-    appendData[name] = name
+  for (let i = 0; i < appends.length; i++) {
+    appendData[appends[i].key] = appends[i].value
   }
 
   if (appends.length && merges.length) {
@@ -121,6 +160,7 @@ async function main() {
   readFiles('models')
 
   let result = parseQuery({
+    name: 'tweeter',
     model: 'Tweet',
     method: 'find',
     filter: {
@@ -136,22 +176,33 @@ async function main() {
     offset: 1,
     select: 'text',
     populate: [{
-        // name: 'user',
+        name: 'fred',
+        // merge: true,
         model: 'User',
-        computed: true,
+        method: 'findOne',
         filter: {
+          _key: '@{tweeter.user}',
           active: true
         },
+        computed: true,
         select: 'firstName lastName',
-        // merge: true
       },
       {
+        name: 'comment',
+        append: 'comments',
         model: 'Comment',
+        filter: {
+          _key: '@{tweeter.user}'
+        },
         limit: 10,
         computed: true,
         populate: [{
-          // name: 'user',
+          name: 'user',
           model: 'User',
+          method: 'findOne',
+          filter: {
+            _key: '@{comment.user}'
+          },
           computed: true,
           select: 'firstName lastName'
         }]
