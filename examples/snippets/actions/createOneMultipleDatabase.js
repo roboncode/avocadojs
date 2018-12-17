0d2ec1167cf93e0e1b674e0a15c9a608ba369467
Rob Taylor <robert.taylor2@vivint.com>
2018-12-17 13:07:00 -0700

    ensure db for createOneMutipleDatabase

--- examples/snippets/actions/createOneMultipleDatabase.js
+++ examples/snippets/actions/createOneMultipleDatabase.js
@@ -1,38 +1,51 @@
 let orango = require('../../../lib')
 const UserMultiDB = require('../models/UserMultiDB')
 
-async function connectDatabase(dbName){
-    const db = orango.get(dbName)
-    if(!db.connection.connected){
-        db.events.on('connected', (instanace) => {
-        console.log(`Conneted to ${instanace.name}`)
-        })
+async function connectDatabase(dbName) {
+  const db = orango.get(dbName)
+  if (!db.connection.connected) {
+    db.events.on('connected', (instanace) => {
+      console.log(`Conneted to ${instanace.name}`)
+    })
     await db.connect(dbName)
     UserMultiDB(db)
-    }else{
-        console.log(`Getting ${db.connection.name}`)
-    }
-    return db
+  } else {
+    console.log(`Getting ${db.connection.name}`)
+  }
+  return db
+  }
+
+async function ensureDatabases() {
+  let User
+  await orango.connect()
+  await orango.createDatabase('sample1')
+  User = await orango.model('User')
+  User.truncate()
+
+  await orango.createDatabase('sample2')
+  User = await orango.model('UserMultiDB')
+  User.truncate()
 }
 
-
 async function main() {
-  for(let i=0; i<10000; i++){
-      if(i%2==0){
-        orango = await connectDatabase('sample2')
-        let User = orango.model('User')
-        let user = new User({
-          email: `user${i}@sample.com`
-        })
-        await user.save()
-      }else{
-        orango = await connectDatabase('sample3')
-        let User = orango.model('User')
-        let user = new User({
-          email: `user${i}@sample.com`
-        })
-        await user.save()
-      }
+  ensureDatabases()
+
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+      orango = await connectDatabase('sample1')
+      let User = await orango.model('User')
+      let user = new User({
+        email: `user${i}@sample.com`
+      })
+      await user.save()
+    } else {
+      orango = await connectDatabase('sample2')
+      let User = await orango.model('User')
+      let user = new User({
+        email: `user${i}@sample.com`
+      })
+      await user.save()
+    }
   }
 }
 
--- examples/snippets/models/UserMultiDB.js
+++ examples/snippets/models/UserMultiDB.js
@@ -1,47 +1,51 @@
 const User = (orango) => {
-    const UserSchema = orango.Schema(
-        {
-            name: String,
-            email: { type: String, required: 'create' },
-            settings: {
-                locale: {
-                    type: String,
-                    default: 'en_US'
-                }
-            },
-            stats: {
-                friends: { type: orango.Types.Any, default: 0 },
-                posts: { type: orango.Types.Any, default: 0 }
-            },
-            created: Date,
-            updated: Date
-        },
-        {
-            strict: true,
-            indexes: [
-                {
-                    type: 'hash',
-                    fields: ['email']
-                }
-            ]
-        }
-    )
+  const UserSchema = orango.Schema({
+    name: String,
+    email: {
+      type: String,
+      required: 'create'
+    },
+    settings: {
+      locale: {
+        type: String,
+        default: 'en_US'
+      }
+    },
+    stats: {
+      friends: {
+        type: orango.Types.Any,
+        default: 0
+      },
+      posts: {
+        type: orango.Types.Any,
+        default: 0
+      }
+    },
+    created: Date,
+    updated: Date
+  }, {
+    strict: true,
+    indexes: [{
+      type: 'hash',
+      fields: ['email']
+    }]
+  })
 
-    UserSchema.computed.username = function (payload) {
-        return this.email.replace(/(\w+)@(\w+).*/g, '$1_$2')
-    }
+  UserSchema.computed.username = function (payload) {
+    return this.email.replace(/(\w+)@(\w+).*/g, '$1_$2')
+  }
 
-    const UserModel = orango.model('User', UserSchema, 'users')
+  const UserModel = orango.model('User', UserSchema, 'users')
 
-    UserModel.on('create', (payload) => {
-        payload.data.created = Date.now()
-    })
+  UserModel.on('create', (payload) => {
+    payload.data.created = Date.now()
+  })
 
-    UserModel.on('update', (payload) => {
-        payload.data.updated = Date.now()
-    })
+  UserModel.on('update', (payload) => {
+    payload.data.updated = Date.now()
+  })
 
-    return UserModel
+  return UserModel
 }
 
 
