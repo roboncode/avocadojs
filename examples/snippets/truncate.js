fa1e78bdec790a1a5f912f6c1066f3ad4aecc42d
Rob Taylor <roboncode@gmail.com>
2018-12-26 10:41:43 -0700

    updated truncate

---
+++ examples/snippets/truncate.js
@@ -0,0 +1,15 @@
+module.exports = async db => {
+  // get a reference to User model
+  const User = db.model('User')
+
+  // create query
+  let query = User.truncate()
+
+  // FOR DEMO ONLY - show the AQL
+  let aql = await query.toAQL(true)
+  console.log(aql.cyan)
+
+  // // find first item in users collection
+  let rawData = await query.exec()
+  console.log('rawData'.green, rawData)
+}
